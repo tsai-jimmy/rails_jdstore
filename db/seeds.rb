a838db5ad98a651a4d766762eecda66ffabadef4
tsai-jimmy <qaz3182@gmail.com>
2018-06-18 00:50:02 +0800

    add is_admin seed & beauty rails console

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-u = User.new
-u.email = "admin@test.com"           # 可以改成自己的 email
+if User.find_by(email: "admin@test.com").nil?
+  u = User.new
+  u.email = "admin@test.com"           # 可以改成自己的 email
 
-u.password = "123456"                # 最少要六码
+  u.password = "123456"                # 最少要六码
 
-u.password_confirmation = "123456"   # 最少要六码
+  u.password_confirmation = "123456"   # 最少要六码
 
-u.is_admin = true
-u.save
+  u.is_admin = true
+  u.save
+  puts "Admin 建立好，帳號為 #{u.email}，密碼為#{u.password}"
+else
+  puts "admin 已經建立過了，腳本跳過該步驟"
+
+end
+Product.create!(
+  title: "Ruby on Rails",
+  description: "ROR is Coming",
+  price: 2222800,
+  quantity: 10,
+  image: open("https://kaochenlong.com/images/books/rails.jpg")
+)
